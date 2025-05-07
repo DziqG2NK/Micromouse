89a8cb42bdc42814128e18b4d3427d0905a58269
DziqG2NK <mracin.walendzik.dysk@gmail.com>
2025-05-07 17:54:35 +0200

    It measures!

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 print("Hello world!")
+
+from machine import Pin, time_pulse_us
+import time
+
+trigger = Pin(11, Pin.OUT)
+echo = Pin(8, Pin.IN)
+
+def measure_distance():
+
+    trigger.low()
+    time.sleep_us(2)
+    trigger.high()
+    time.sleep_us(10)
+    trigger.low()
+
+    # Measure echo pulse width
+    duration = time_pulse_us(echo, 1, 30000)  # 30 ms timeout
+    if duration < 0:
+        return None  # timeout
+
+    distance_cm = duration / 58.0  # według dokumentacji SRF05
+    return distance_cm
+
+while True:
+    dist = measure_distance()
+    if dist is not None:
+        print("Odległość: {:.2f} cm".format(dist))
+    else:
+        print("Brak echa")
+    time.sleep(0.5)
