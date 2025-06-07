a9d06d0eb1f25e5afd85eb22efa6e941748dec77
Dominik Dróżdż <ddrozdz@student.agh.edu.pl>
2025-06-07 17:51:57 +0200

    simulation logic in vehicle

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -5,6 +5,9 @@ from map import Map
 
 # loading map
 
+def all_measurements():
+    for dir in dirs:
+        print(f"{dir}: {car.measure_distance(dir)}")
 
 
 map = Map(r'C:\Users\Domin\Documents\GitHub\Micromouse\simulation\example_map.png')
@@ -19,11 +22,17 @@ sensor_r = vSensor(Direction.RIGHT, map)
 
 car = Vehicle(sensor_l, sensor_u, sensor_r)
 car.set_pos(*map.get_start())
+car.you_are_in_the_Matrix_Neo()
 
 sensor_l.load_vehicle(car)
 sensor_u.load_vehicle(car)
 sensor_r.load_vehicle(car)
 
-for dir in dirs:
-    print(f"{dir}: {car.measure_distance(dir)}")
+all_measurements()
 
+car.set_direction(Direction.RIGHT)
+
+all_measurements()
+
+car.ride_forward(30)
+all_measurements()
--- vehicle.py
+++ vehicle.py
@@ -1,6 +1,7 @@
 from math import sin, cos
 from directions import Direction
 
+
 class Vehicle:
     def __init__(self, sensor_left, sensor_up, sensor_right):
         self.x = 0
@@ -9,6 +10,7 @@ class Vehicle:
         self.sensor_left = sensor_left
         self.sensor_up = sensor_up
         self.sensor_right = sensor_right
+        self.is_simulation = False
 
     def measure_distance(self, direction):
         if direction == Direction.UP:
@@ -19,18 +21,47 @@ class Vehicle:
             return self.sensor_left.measure()
 
     def ride_forward(self, distance):
+        # Na ten moment taki kod do testowania symulacji, bo nie obsługuje silnika.
+        if self.is_simulation:
+            if self.dir == Direction.UP:
+                self.y -= distance
+            elif self.dir == Direction.RIGHT:
+                self.x += distance
+            elif self.dir == Direction.DOWN:
+                self.y += distance
+            elif self.dir == Direction.LEFT:
+                self.x -= distance
+            return
+
         start_dist = self.measure_distance(Direction.UP)
         while self.measure_distance(Direction.UP) < start_dist - distance:
             # TODO: Obsługa silnika
             # Kod zakłada że łazik jedzie idealnie w lini prostej i nie wykryje jeżeli zacznie zbliżać się do ściany.
             pass
 
-    def turn(self, direction):
+    def set_direction(self, direction, epsilon= 0.1):
+        # Na ten moment taki kod do testowania symulacji, bo nie obsługuje silnika.
+        if self.is_simulation:
+            self.dir = direction
+            return
+
         degrees_to_turn = (direction.value - self.dir.value) * 90
 
+        m_left = self.measure_distance(Direction.LEFT)
+        m_up = self.measure_distance(Direction.UP)
+        m_right = self.measure_distance(Direction.RIGHT)
+
         # TODO: Obsługa silnika
+
+
+
+
+        self.dir = direction
 
 
     def set_pos(self, x, y):
         self.x = x
         self.y = y
+
+    def you_are_in_the_Matrix_Neo(self):
+        self.is_simulation = True
