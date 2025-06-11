0ae5f17ed184f367d29316e5f8158370042699d6
DziqG2NK <mracin.walendzik.dysk@gmail.com>
2025-06-11 04:14:49 +0200

    Forward and rotate back functions

--- motors.py
+++ motors.py
@@ -20,19 +20,72 @@ while True:
     print("Pin 14: 0, Pin 15: 1")
     time.sleep(3)"""
     
-def forward(AIN1_pin, AIN2_pin, BIN1_pin, BIN2_pin):
+def stop(AIN1_pin, AIN2_pin, BIN1_pin, BIN2_pin):
+    AIN1.value(0)
+    AIN2.value(0)
+    
+    BIN1.value(0)
+    BIN2.value(0)
+    
+    print("Engines stopped")
+    
+    
+def right(AIN1_pin, AIN2_pin, BIN1_pin, BIN2_pin):
     AIN1.value(1)
     AIN2.value(0)
     
     BIN1.value(1)
     BIN2.value(0)
     
+    print("Turning right")
+    
+    time.sleep(0.5)
+    
+    stop(AIN1_pin, AIN2_pin, BIN1_pin, BIN2_pin)
+
+def left(AIN1_pin, AIN2_pin, BIN1_pin, BIN2_pin):
+    AIN1.value(0)
+    AIN2.value(1)
+    
+    BIN1.value(0)
+    BIN2.value(1)
+    
+    print("Turning left")
+
+    time.sleep(0.5)
+    
+    stop(AIN1_pin, AIN2_pin, BIN1_pin, BIN2_pin)
+
+def rotate_back(AIN1_pin, AIN2_pin, BIN1_pin, BIN2_pin):
+    AIN1.value(0)
+    AIN2.value(1)
+    
+    BIN1.value(0)
+    BIN2.value(1)
+    
+    print("Turning back")
+
     time.sleep(0.1)
     
+    stop(AIN1_pin, AIN2_pin, BIN1_pin, BIN2_pin)
+
+def forward(AIN1_pin, AIN2_pin, BIN1_pin, BIN2_pin):
     AIN1.value(0)
-    AIN2.value(0)
-
-    BIN1.value(0)
+    AIN2.value(1)
+    
+    BIN1.value(1)
     BIN2.value(0)
+    
+    print("Driving forward")
 
-forward(AIN1, AIN2, BIN1, BIN2)
+    time.sleep(0.1)
+    
+    stop(AIN1_pin, AIN2_pin, BIN1_pin, BIN2_pin)
+
+#right(AIN1, AIN2, BIN1, BIN2)
+
+time.sleep(2)
+
+#left(AIN1, AIN2, BIN1, BIN2)
+
+#forward(AIN1, AIN2, BIN1, BIN2)
